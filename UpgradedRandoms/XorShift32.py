f82f5ee04cade53bd6f640d9e937d4cef12023ac
Jiwootv <yooziwoo.coding0987@gmail.com>
2026-01-23 16:53:55 +0900

    두번째 커밋 (함수 몇개 좀 추가함)

--- UpgradedRandoms/XorShift32.py
+++ UpgradedRandoms/XorShift32.py
@@ -12,3 +12,15 @@ class Xorshift32:
 
 	def next_float(self):
 		return self.next_u32() / 2**32
+
+	def get_value_list(self, len: int, type: str):
+		"""
+		len: 값의 길이
+		Type: 타입. u32 또는 float을 선택 가능합니다.
+		"""
+		self.l = []
+		for i in range(len):
+			if type == "int": self.l.append(self.next_int())
+			elif type == "float": self.l.append(self.next_float())
+			else: raise ValueError(f"잘못된 Type값입니다. u32 또는 float을 입력하십시오. 현재 {type}이/가 입력되었습니다.")
+		print(self.l)
