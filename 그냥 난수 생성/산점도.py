1df9a0fc3018b4026a09a384539f859f37695237
jiwootv <81001562+jiwootv@users.noreply.github.com>
2026-02-01 21:35:15 +0900

    Update 산점도.py

--- 그냥 난수 생성/산점도.py
+++ 그냥 난수 생성/산점도.py
@@ -5,11 +5,11 @@ pos = [_-_ for _ in range(1, 696969)]
 # [0, 0, ..., 0]
 # [(0, 0), (0,0)
 
-def angang(sibal):
-	for i in range(sibal):
+def hehehehehe(l):
+	for i in range(l):
 		random.seed(i)
-		pos[i]  = (i, random.randint(1, sibal+1))
-	plt.scatter([pos[k][0] for k in range(1, sibal)], [pos[k][1] for k in range(1, sibal)])
+		pos[i]  = (i, random.randint(1, l+1))
+	plt.scatter([pos[k][0] for k in range(1, l)], [pos[k][1] for k in range(1, l)])
 	plt.show()
 
-angang(69)
+hehehehehe(69)
